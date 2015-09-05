a204a86ee4b3f7ff36f6267a45095e19f5ffd0b4
Administrator <Administrator@B5A6F3A51D4F4EE>
2015-09-05 20:57:55 +0800

    增加语音聊天功能

--- src/com/ucpaas/chat/activity/ConversationActivity.java
+++ src/com/ucpaas/chat/activity/ConversationActivity.java
@@ -1,14 +1,20 @@
 package com.ucpaas.chat.activity;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
@@ -17,9 +23,14 @@ import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -27,12 +38,14 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ucpaas.chat.R;
 import com.ucpaas.chat.adapter.ConversationReplyAdapter;
 import com.ucpaas.chat.config.AppConstants;
 import com.ucpaas.chat.support.SpOperation;
 import com.ucpaas.chat.support.UcUtils;
+import com.ucpaas.chat.util.AudioRecorder;
 import com.ucpaas.chat.util.LogUtil;
 import com.ucpaas.chat.util.ToastUtil;
 import com.yzxIM.IMManager;
@@ -282,6 +295,9 @@ public class ConversationActivity extends Activity implements MessageListener,
 	private LinearLayout im_ll_record;
 	private LinearLayout im_ll_shot;
 
+	private ImageView img_record_btn;
+	private TextView textView_record_time;
+	
 	private void initBottonMenu() {
 		mLayoutBottom = (LinearLayout) findViewById(R.id.ll_bottom);
 		mBtnMore = (RelativeLayout) findViewById(R.id.im_more);
@@ -338,8 +354,254 @@ public class ConversationActivity extends Activity implements MessageListener,
 				}
 			}
 		});
+		
+		//
+		img_record_btn = (ImageView) findViewById(R.id.im_iv_record);
+		img_record_btn.setOnClickListener(this);
+		textView_record_time = (TextView) findViewById(R.id.im_tv_record);
+		textView_record_time.setText("按住说话");
+		
+		img_record_btn.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				startRecord(v, event);
+				return false;
+			}
+		});
 	}
+	
+	/**
+	 * 开始录音
+	 * @param v
+	 * @param event
+	 */
+    private static final int MIN_RECORD_TIME = 1; // 最短录制时间，单位秒，0为无时间限制
 
+    private static final int RECORD_OFF = 0; // 不在录音
+    private static final int RECORD_ON = 1; // 正在录音
+    private static final String RECORD_FILENAME = "record0033"; // 录音文件名
+    private int recordState = 0; // 录音状态
+    private float recodeTime = 0.0f; // 录音时长
+    private double voiceValue = 0.0; // 录音的音量值
+    private boolean playState = false; // 录音的播放状态
+    private boolean moveState = false; // 手指是否移动
+    private float downY;
+    
+    private Dialog mRecordDialog;
+    private AudioRecorder mAudioRecorder;
+    private MediaPlayer mMediaPlayer;
+    private Thread mRecordThread;
+    
+    private TextView mTvRecordDialogTxt;
+    private ImageView mIvRecVolume;
+
+    
+	private void startRecord(View v, MotionEvent event){
+		switch (event.getAction()) {
+        case MotionEvent.ACTION_DOWN: // 按下按钮
+            if (recordState != RECORD_ON) {
+                downY = event.getY();
+                deleteOldFile();
+                mAudioRecorder = new AudioRecorder(RECORD_FILENAME);
+                recordState = RECORD_ON;
+                try {
+                    mAudioRecorder.start();
+                    recordTimethread();
+                    showVoiceDialog(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            break;
+        case MotionEvent.ACTION_MOVE: // 滑动手指
+            float moveY = event.getY();
+            if (downY - moveY > 50) {
+                moveState = true;
+                showVoiceDialog(1);
+            }
+            if (downY - moveY < 20) {
+                moveState = false;
+                showVoiceDialog(0);
+            }
+            break;
+        case MotionEvent.ACTION_CANCEL:
+        case MotionEvent.ACTION_UP: // 松开手指
+            if (recordState == RECORD_ON) {
+                recordState = RECORD_OFF;
+                if (mRecordDialog.isShowing()) {
+                    mRecordDialog.dismiss();
+                }
+                try {
+                    mAudioRecorder.stop();
+                    mRecordThread.interrupt();
+                    voiceValue = 0.0;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (!moveState) {
+                    if (recodeTime < MIN_RECORD_TIME) {
+                        showWarnToast("时间太短  录音失败");
+                    } else {
+                        textView_record_time.setText("录音时间："
+                                + ((int) recodeTime));
+                        String path = getAmrPath();
+                        sendVoiceMessage(path, (int)recodeTime + "");
+//                        mTvRecordPath.setText("文件路径：" + getAmrPath());
+                    }
+                }
+                moveState = false;
+            }
+            break;
+        }
+	}
+	
+	   // 删除老文件
+    void deleteOldFile() {
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "WifiChat/voiceRecord/" + RECORD_FILENAME + ".amr");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+	
+	  // 录音时显示Dialog
+    void showVoiceDialog(int flag) {
+        if (mRecordDialog == null) {
+            mRecordDialog = new Dialog(ConversationActivity.this, R.style.DialogStyle);
+            mRecordDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            mRecordDialog.getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            mRecordDialog.setContentView(R.layout.record_dialog);
+            mIvRecVolume = (ImageView) mRecordDialog.findViewById(R.id.record_dialog_img);
+            mTvRecordDialogTxt = (TextView) mRecordDialog.findViewById(R.id.record_dialog_txt);
+        }
+        switch (flag) {
+        case 1:
+            mIvRecVolume.setImageResource(R.drawable.record_cancel);
+            mTvRecordDialogTxt.setText("松开手指可取消录音");
+            break;
+
+        default:
+            mIvRecVolume.setImageResource(R.drawable.record_animate_01);
+            mTvRecordDialogTxt.setText("向上滑动可取消录音");
+            break;
+        }
+        mTvRecordDialogTxt.setTextSize(14);
+        mRecordDialog.show();
+    }
+    
+    
+    // 录音时间太短时Toast显示
+    void showWarnToast(String toastText) {
+        Toast toast = new Toast(ConversationActivity.this);
+        LinearLayout linearLayout = new LinearLayout(ConversationActivity.this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(20, 20, 20, 20);
+
+        // 定义一个ImageView
+        ImageView imageView = new ImageView(ConversationActivity.this);
+        imageView.setImageResource(R.drawable.voice_to_short); // 图标
+
+        TextView mTv = new TextView(ConversationActivity.this);
+        mTv.setText(toastText);
+        mTv.setTextSize(14);
+        mTv.setTextColor(Color.WHITE);// 字体颜色
+
+        // 将ImageView和ToastView合并到Layout中
+        linearLayout.addView(imageView);
+        linearLayout.addView(mTv);
+        linearLayout.setGravity(Gravity.CENTER);// 内容居中
+        linearLayout.setBackgroundResource(R.drawable.record_bg);// 设置自定义toast的背景
+
+        toast.setView(linearLayout);
+        toast.setGravity(Gravity.CENTER, 0, 0);// 起点位置为中间
+        toast.show();
+    }
+
+ // 获取文件手机路径
+    private String getAmrPath() {
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "WifiChat/voiceRecord/" + RECORD_FILENAME + ".amr");
+        return file.getAbsolutePath();
+    }
+
+    // 录音计时线程
+    void recordTimethread() {
+        mRecordThread = new Thread(recordThread);
+        mRecordThread.start();
+    }
+
+    // 录音Dialog图片随声音大小切换
+    void setDialogImage() {
+        if (voiceValue < 600.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_01);
+        } else if (voiceValue > 600.0 && voiceValue < 1000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_02);
+        } else if (voiceValue > 1000.0 && voiceValue < 1200.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_03);
+        } else if (voiceValue > 1200.0 && voiceValue < 1400.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_04);
+        } else if (voiceValue > 1400.0 && voiceValue < 1600.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_05);
+        } else if (voiceValue > 1600.0 && voiceValue < 1800.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_06);
+        } else if (voiceValue > 1800.0 && voiceValue < 2000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_07);
+        } else if (voiceValue > 2000.0 && voiceValue < 3000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_08);
+        } else if (voiceValue > 3000.0 && voiceValue < 4000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_09);
+        } else if (voiceValue > 4000.0 && voiceValue < 6000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_10);
+        } else if (voiceValue > 6000.0 && voiceValue < 8000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_11);
+        } else if (voiceValue > 8000.0 && voiceValue < 10000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_12);
+        } else if (voiceValue > 10000.0 && voiceValue < 12000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_13);
+        } else if (voiceValue > 12000.0) {
+            mIvRecVolume.setImageResource(R.drawable.record_animate_14);
+        }
+    }
+
+    // 录音线程
+    private Runnable recordThread = new Runnable() {
+
+        @Override
+        public void run() {
+            recodeTime = 0.0f;
+            while (recordState == RECORD_ON) {
+                // 限制录音时长
+                // if (recodeTime >= MAX_RECORD_TIME && MAX_RECORD_TIME != 0) {
+                // imgHandle.sendEmptyMessage(0);
+                // } else
+                {
+                    try {
+                        Thread.sleep(150);
+                        recodeTime += 0.15;
+                        // 获取音量，更新dialog
+                        if (!moveState) {
+                            voiceValue = mAudioRecorder.getAmplitude();
+                            recordHandler.sendEmptyMessage(1);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    };
+
+    public Handler recordHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            setDialogImage();
+        }
+    };
+    
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
--- src/com/ucpaas/chat/adapter/ConversationReplyAdapter.java
+++ src/com/ucpaas/chat/adapter/ConversationReplyAdapter.java
@@ -1,11 +1,14 @@
 package com.ucpaas.chat.adapter;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -23,20 +26,28 @@ import com.yzxIM.data.db.ChatMessage;
 
 /**
  * 聊天记录Adapter
- *
+ * 
  * @author tangqi
  * @date 2015年9月4日上午12:31:28
  */
 public class ConversationReplyAdapter extends BaseAdapter {
 
-	private final int VIEW_TYPE_COUNT = 2;
-	private final int VIEW_TYPE_USER = 0;
-	private final int VIEW_TYPE_DEV = 1;
-
+	private final int VIEW_TYPE_COUNT = 7;
+//	private final int VIEW_TYPE_USER = 0;
+//	private final int VIEW_TYPE_DEV = 1;
+	private final int VIEW_TEXT_DAULT = 0;
+	private final int VIEW_TEXT_L = 1;
+	private final int VIEW_TEXT_R = 2;
+	private final int VIEW_IMG_L = 3;
+	private final int VIEW_IMG_R = 4;
+	private final int VIEW_VOICE_L = 5;
+	private final int VIEW_VOICE_R = 6;
+	
 	private List<ChatMessage> mList;
 	private Context mContext;
 
-	public ConversationReplyAdapter(Context context, List<ChatMessage> mChatMessages) {
+	public ConversationReplyAdapter(Context context,
+			List<ChatMessage> mChatMessages) {
 		this.mList = mChatMessages;
 		this.mContext = context;
 	}
@@ -65,75 +76,173 @@ public class ConversationReplyAdapter extends BaseAdapter {
 	@Override
 	public int getItemViewType(int position) {
 		// 获取单条回复
+		int type = 0;
 		ChatMessage chatMessage = getItem(position);
-		if (chatMessage.getIsFromMyself()) {
-			// 开发者回复Item布局
-			return VIEW_TYPE_DEV;
-		} else {
-			// 用户反馈、回复Item布局
-			return VIEW_TYPE_USER;
+		if (chatMessage.getMsgType() == MSGTYPE.MSG_DATA_TEXT) {		
+			if (chatMessage.getIsFromMyself()) {
+				// 开发者回复Item布局
+				type = VIEW_TEXT_R;
+			} else {
+				// 用户反馈、回复Item布局
+				type = VIEW_TEXT_L;
+			}
+		}else if (chatMessage.getMsgType() == MSGTYPE.MSG_DATA_IMAGE) {		
+			if (chatMessage.getIsFromMyself()) {
+				// 开发者回复Item布局
+				type = VIEW_IMG_R;
+			} else {
+				// 用户反馈、回复Item布局
+				type = VIEW_IMG_L;
+			}
+		}else if(chatMessage.getMsgType() == MSGTYPE.MSG_DATA_IMAGE) {
+			if (chatMessage.getIsFromMyself()) {
+				// 开发者回复Item布局
+				type = VIEW_VOICE_R;
+			} else {
+				// 用户反馈、回复Item布局
+				type = VIEW_VOICE_L;
+			}
 		}
-
+		return type;
 	}
+
+	private ViewHolder holder = null;
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		ViewHolder holder = null;
-		// 获取单条回复
-		final ChatMessage chatMessage = getItem(position);
-		if (convertView == null) {
-			// 根据Type的类型来加载不同的Item布局
 
-			if (chatMessage.getIsFromMyself()) {
-				// 开发者的回复
-				convertView = LayoutInflater.from(mContext).inflate(R.layout.umeng_fb_custom_dev_reply, null);
-			} else {
-				// 用户的反馈、回复
-				convertView = LayoutInflater.from(mContext).inflate(R.layout.umeng_fb_custom_user_reply, null);
+		ChatMessage chatMessage = getItem(position);
+		convertView = initConvertView(position, convertView , chatMessage);
+
+		setData(position, chatMessage);
+
+		return convertView;
+	}
+
+	/**
+	 * 
+	 */
+	private View initConvertView(int position, View convertView, ChatMessage chatMessage) {
+		// 获取单条回复
+		
+		if (convertView == null ) {
+			// 根据Type的类型来加载不同的Item布局
+			
+			
+			
+			// 文本消息
+			if (chatMessage.getMsgType() == MSGTYPE.MSG_DATA_TEXT) {
+				if (chatMessage.getIsFromMyself()) {
+					// 本人发送
+					convertView = LayoutInflater.from(mContext).inflate(
+							R.layout.conversation_text_right, null);
+				} else {
+					// 他人回复
+					convertView = LayoutInflater.from(mContext).inflate(
+							R.layout.conversation_text_left, null);
+				}
+				// 图片消息
+			} else if (chatMessage.getMsgType() == MSGTYPE.MSG_DATA_IMAGE) {
+				if (chatMessage.getIsFromMyself()) {
+					// 本人发送
+					convertView = LayoutInflater.from(mContext).inflate(
+							R.layout.conversation_image_right, null);
+				} else {
+					// 他人回复
+					convertView = LayoutInflater.from(mContext).inflate(
+							R.layout.conversation_image_left, null);
+				}
+
+				// 语音消息
+			} else if (chatMessage.getMsgType() == MSGTYPE.MSG_DATA_VOICE) {
+				if (chatMessage.getIsFromMyself()) {
+					// 本人发送
+					convertView = LayoutInflater.from(mContext).inflate(
+							R.layout.conversation_voice_right, null);
+				} else {
+					// 他人回复
+					convertView = LayoutInflater.from(mContext).inflate(
+							R.layout.conversation_voice_left, null);
+				}
+			}else {
+				if (chatMessage.getIsFromMyself()) {
+					// 本人发送
+					convertView = LayoutInflater.from(mContext).inflate(
+							R.layout.conversation_text_right, null);
+				} else {
+					// 他人回复
+					convertView = LayoutInflater.from(mContext).inflate(
+							R.layout.conversation_text_left, null);
+				}
 			}
 
 			// 创建ViewHolder并获取各种View
 			holder = new ViewHolder();
-			holder.replyContent = (TextView) convertView.findViewById(R.id.fb_reply_content);
-			holder.replyImgSmall = (ImageView) convertView.findViewById(R.id.img_small_pic);
-			holder.replyProgressBar = (ProgressBar) convertView.findViewById(R.id.fb_reply_progressBar);
-			holder.replyStateFailed = (ImageView) convertView.findViewById(R.id.fb_reply_state_failed);
-			holder.replyData = (TextView) convertView.findViewById(R.id.fb_reply_date);
+	
+				holder.replyContent = (TextView) convertView
+						.findViewById(R.id.fb_reply_content);
+				holder.replyProgressBar = (ProgressBar) convertView
+						.findViewById(R.id.fb_reply_progressBar);
+				holder.replyStateFailed = (ImageView) convertView
+						.findViewById(R.id.fb_reply_state_failed);
+				holder.replyData = (TextView) convertView
+						.findViewById(R.id.fb_reply_date);
+
+				// 图片
+				holder.replyImgSmall = (ImageView) convertView
+						.findViewById(R.id.img_small_pic);
+
+
+
+				// 语音
+				holder.imgVoiceSrc = (ImageView) convertView
+						.findViewById(R.id.img_voice);
+				holder.txvVoiceTime = (TextView) convertView
+						.findViewById(R.id.tv_voice_time);
+
+			
 			convertView.setTag(holder);
 
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
+		return convertView;
+	}
 
-		// 以下是填充数据
+	/**
+	 * 
+	 * @param position
+	 */
+	private boolean playStatus = false;
+	private MediaPlayer mMediaPlayer;
+	
+	private void setData(int position, ChatMessage chatMessage) {
+
+		// 文本消息
 		if (chatMessage.getMsgType() == MSGTYPE.MSG_DATA_TEXT) {
 			holder.replyContent.setText(chatMessage.getContent());
-			holder.replyContent.setVisibility(View.VISIBLE);
-			holder.replyImgSmall.setVisibility(View.GONE);
+			// 图片消息
 		} else if (chatMessage.getMsgType() == MSGTYPE.MSG_DATA_IMAGE) {
-			holder.replyContent.setText(chatMessage.getContent());
-			holder.replyContent.setVisibility(View.GONE);
-			holder.replyImgSmall.setVisibility(View.VISIBLE);
-			ImageLoaderUtils.displaySdcardImg(chatMessage.getContent(), holder.replyImgSmall);
+			ImageLoaderUtils.displaySdcardImg(chatMessage.getContent(),
+					holder.replyImgSmall);
 
+			final ChatMessage data = chatMessage;
 			holder.replyImgSmall.setOnClickListener(new OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
-					// TODO Auto-generated method stub
 					Intent intent = new Intent(mContext, ImageActivity.class);
-					intent.putExtra("chatMessage", chatMessage);
+					intent.putExtra("chatMessage", data);
 					mContext.startActivity(intent);
 				}
 			});
+			// 语音消息
 		} else if (chatMessage.getMsgType() == MSGTYPE.MSG_DATA_VOICE) {
-			holder.replyContent.setText(chatMessage.getContent());
-			holder.replyContent.setVisibility(View.VISIBLE);
-			holder.replyImgSmall.setVisibility(View.GONE);
+			String str = chatMessage.getContent();
+			holder.txvVoiceTime.setText(chatMessage.getContent() + "\"");
+			playRecord(chatMessage);
 		} else {
-			holder.replyContent.setText(chatMessage.getContent());
-			holder.replyContent.setVisibility(View.VISIBLE);
-			holder.replyImgSmall.setVisibility(View.GONE);
+	
 		}
 
 		// 在App应用界面，对于开发者的Reply来讲status没有意义
@@ -163,7 +272,54 @@ public class ConversationReplyAdapter extends BaseAdapter {
 				holder.replyData.setVisibility(View.VISIBLE);
 			}
 		}
-		return convertView;
+	}
+	
+	/**
+	 * 
+	 */
+	private void playRecord(ChatMessage chatMessage){
+		//播放语音
+		final ChatMessage data = chatMessage;
+		holder.imgVoiceSrc.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (!playStatus) {
+					mMediaPlayer  = new MediaPlayer();
+					try {
+						mMediaPlayer.setDataSource(data.getPath());
+						mMediaPlayer.prepare();
+						playStatus = true;
+						mMediaPlayer.start();
+						mMediaPlayer.setOnCompletionListener(new OnCompletionListener() {
+							
+							@Override
+							public void onCompletion(MediaPlayer mp) {
+								if (playStatus) {
+									playStatus = false;
+								}
+							}
+						});
+						
+					} catch (IllegalArgumentException e) {
+						e.printStackTrace();
+					} catch (SecurityException e) {
+						e.printStackTrace();
+					} catch (IllegalStateException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}else {
+					if (mMediaPlayer.isPlaying()) {
+						mMediaPlayer.stop();
+						playStatus = false;
+					}else {
+						playStatus = false;
+					}
+				}
+			}
+		});
 	}
 
 	class ViewHolder {
@@ -171,5 +327,10 @@ public class ConversationReplyAdapter extends BaseAdapter {
 		ProgressBar replyProgressBar;
 		ImageView replyStateFailed, replyImgSmall;
 		TextView replyData;
+
+		// voice
+		ImageView imgVoiceSrc;
+		TextView txvVoiceTime;
+
 	}
 }
